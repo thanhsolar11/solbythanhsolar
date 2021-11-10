2d11530be71a522ae601926c97ef9cf769e361cc
Thanh Solar <phamthanh240818@gmail.com>
2021-11-10 09:10:10 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+setTimeout (function(){
+    confirm("Hãy xác nhận đây là chị Linh của tui !");
+}, 1000)
+// setInterval (function(){
+//     console.log('Em yêu chị =)))' + Math.random());
+// }, 100)
